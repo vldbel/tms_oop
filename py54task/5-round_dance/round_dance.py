47216100978adc2084ada146ef21076f04501030
vldbel <vldbel@gmail.com>
2024-06-29 20:47:04 +0000

    round dance was completely redesigned into circular linked list

--- py54task/5-round_dance/round_dance.py
+++ py54task/5-round_dance/round_dance.py
@@ -1,35 +1,64 @@
+"""Circular Singly linked list (forward connection)"""
+
 class Child:
-    children = []
-    children_count = 0
-    
-    @classmethod
-    def _register_child(cls, self):
-        cls.children.append(self)
-    
-    @classmethod
-    def round_dance(cls):
-        for idx in range(len(cls.children)):
-            cls.children[idx].next = cls.children[idx + 1] if (idx + 1) < len(cls.children) else cls.children[0]
-    
-    def __init__(self):
+    def __init__(self, name):
         self.next = None
-        Child._register_child(self)
-        Child.children_count += 1
-
-
+        self.name = name
+        
+    def __str__(self):
+        return self.name
+        
+        
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        
+    def prepend(self, name):
+        new_node = Child(name)
+        new_node.next = self.head
+        self.head = new_node
+        self.tail.next = self.head
+        
+    def append(self, name):
+        new_node = Child(name)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+        self.tail.next = self.head
+    
+    def remove(self, name):
+        ...
+    
+    def pop(self, name):
+        ...
+                    
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(f"{current_node} ({current_node.next})", end=" -> " if current_node.next != self.tail.next else '\n')
+            current_node = current_node.next
+            if current_node == self.head: # last node
+                break
+            
+    
 def test():
-    ch1 = Child()
-    ch2 = Child()
-    ch3 = Child()
+    Circ_lst = CircularLinkedList()
+    Circ_lst.append("Sasha")
+    Circ_lst.append("Angelina")
+    Circ_lst.append("Jadviga")
+    Circ_lst.append("Maksim")
+    Circ_lst.append("Vova")
+    Circ_lst.prepend("Petr")
+    Circ_lst.print_list()
+    # print(f"Head: {Circ_lst.head}")
+    # print(f"Tail: {Circ_lst.tail}")
+    
 
-    assert Child.children_count == 3
-    assert len(Child.children) == 3
-    
-    Child.round_dance()
-    assert ch1.next == ch2
-    assert ch2.next == ch3
-    assert ch3.next == ch1
-    
-    
 if __name__ == "__main__":
     test()
+    
+# https://www.youtube.com/watch?v=Exol2HWE97k - great video about about the topic 
